cafac7b04f4c1baa0df11b7ca2b38ac0a32ee139
Magzhan Zhumaly <magzhanzhumaly@gmail.com>
2024-05-06 12:14:07 +0500

    Initial Commit

---
+++ AuthorizationAezakmi/AuthorizationAezakmiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthorizationAezakmiApp.swift
+//  AuthorizationAezakmi
+//
+//  Created by Magzhan Zhumaly on 06.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct AuthorizationAezakmiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
